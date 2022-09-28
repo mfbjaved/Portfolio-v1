d88fa8f3b7d73c04562b38700ce3ec8f72675a24
Madiha Javed <mfbjaved@gmail.com>
2022-09-28 11:59:36 -0700

    basic hero page updated

---
+++ frontend/src/constants/images.js
@@ -0,0 +1,9 @@
+import cartoonSelf from '../assest/cartoonSelf.PNG';
+import pinkCircle from '../assest/pinkCircle.png';
+import pinkRadialGradient from '../assest/pinkRadialGradient.png';
+
+export default {
+    cartoonSelf,
+    pinkCircle,
+    pinkRadialGradient
+}
