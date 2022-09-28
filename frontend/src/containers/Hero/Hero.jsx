a566b8d53eb252a2a5631c4ebca4236c3ec4b9d9
Madiha Javed <mfbjaved@gmail.com>
2022-09-28 12:16:36 -0700

    hero section draft

--- frontend/src/containers/Hero/Hero.jsx
+++ frontend/src/containers/Hero/Hero.jsx
@@ -13,15 +13,15 @@ const Hero = () => {
           <div className='app__hero-socialinks'>
           </div>
           <p className='app__hero-secondarytext'>
-          I am a life long learner, polyglot, and hobbyist photographer. I love being crafty taking up new projects ranging from sewing to crochet. 
-          {/* <a href="#">Read full about me.</a> */}
+          I am a life long learner, polyglot, and hobbyist photographer. I love being crafty taking up new projects ranging from sewing to crochet.  <a href="#">Read full about me.</a>
           </p>
         </div>
         <div className='app__hero-image'>
           <img id='selfImage' src={images.cartoonSelf} alt="Cartoonized Madiha"/>
           <img id="pinkCircle" src={images.pinkCircle} alt="Pink Circle"/>
-          {/* <img id="pinkGraident" src={images.pinkRadialGradient} alt="Pink Gradient"/> */}
+          
         </div>
+        <img id="pinkGraident" src={images.pinkRadialGradient} alt="Pink Gradient"/>
       </div>
     </div>
   )
